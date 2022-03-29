955873ca7b2d5deb51e108e43627449a45a4e791
Bruno Bandeira <brunoban.dev@gmail.com>
2022-03-29 18:33:46 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Halpert",
+		contactInfo: contactInfo{
+			email:   "jim@halpert.com",
+			zipCode: 910983,
+		},
+	}
+
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
