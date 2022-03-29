798d0b7a2eab82dbeda12af69080acab29967506
Bruno Bandeira <brunoban.dev@gmail.com>
2022-03-29 19:24:12 +0100

    added some notes

--- main.go
+++ main.go
@@ -23,15 +23,47 @@ func main() {
 		},
 	}
 
-	jimPointer := &jim
-	jimPointer.updateName("Jimmy")
+	// Ref a memory address (&jim)
+	// jimPointer := &jim
+	// jimPointer.updateName("Jimmy")
+
+	// Or simply
+	jim.updateName("Jimmy")
 	jim.print()
 }
 
 func (pointerToPerson *person) updateName(newFirstName string) {
+	// Get a value of memory address (*pointerToPerson)
 	(*pointerToPerson).firstName = newFirstName
 }
 
 func (p person) print() {
 	fmt.Printf("%+v", p)
 }
+
+// |===============================================
+// | Memory Address  | Value
+// |===============================================
+// | 0001            | person{firstName: "bruno"}
+// |-----------------------------------------------
+// | 0002            |
+// |-----------------------------------------------
+// | 0003            | person{firstName: "bruno"}
+// |-----------------------------------------------
+// | ...
+
+// Turn ADDRESS into VALUE with *address
+// Turn VALUE into ADDRESS with &value
+
+// =================================
+// | value types | reference types |
+// =================================
+// | int         | slices          |
+// | float       | maps            |
+// | string      | channels        |
+// | bool        | pointers        |
+// | structs     | functions       |
+// =================================
+
+// Value types = Use pointers to change these things in a function
+// Reference types = Dont worry with pointers with these
